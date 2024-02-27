cc17db074e184dcf27a4ecc8a6ad61e9ecce569c
Gabriel <maldoangel@hotmail.com>
2024-02-27 15:36:51 -0300

    categories with graphql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -7,15 +7,41 @@ import { CategoriesContext } from "../../contexts/categories.context";
 
 import { CategoryContainer, Title } from "./category.styles";
 import Spinner from "../../components/spinner/spinner.component";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        name
+        id
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: { title: category },
+  });
+
+  //const { categoriesMap, loading } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [data, category]);
 
   if (loading) return <Spinner />;
 
